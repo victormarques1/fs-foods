a89334f250e1e690b3a5d3e76f401ae43dc3fd97
Victor Marques <marques.vct@hotmail.com>
2024-08-27 20:40:56 -0300

    feat: add recommended products page

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ const Home = async () => {
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos recomendados</h2>
-          <Link href="/restaurants/recommended">
+          <Link href="/products/recommended">
             <Button
               variant="ghost"
               className="h-fit p-0 text-primary hover:bg-transparent"
@@ -70,13 +70,15 @@ const Home = async () => {
       <div className="space-y-4 py-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Restaurantes recomendados</h2>
-          <Button
-            variant="ghost"
-            className="h-fit p-0 text-primary hover:bg-transparent"
-          >
-            Ver todos
-            <ChevronRightIcon size={16} />
-          </Button>
+          <Link href="/restaurants/recommended">
+            <Button
+              variant="ghost"
+              className="h-fit p-0 text-primary hover:bg-transparent"
+            >
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         </div>
         <RestaurantList />
       </div>
